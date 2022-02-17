d1a1985a9f61a937382b4f2d8e6abc62cd6dc0b2
Milenko Stanković <stankovicm1999@gmail.com>
2022-02-17 01:05:34 +0100

    Finished

--- js/main.js
+++ js/main.js
@@ -12,7 +12,24 @@ setBar();
 
 
 let val = document.querySelector('#rangeValue');
+let pg = document.querySelector('#pageViewsNumbers');
 
 function rangeSlide(value) {
-  val.innerHTML = '$' + value;
+  val.innerHTML = '$' + (Math.round(value * 100) / 100).toFixed(2);;
+
+	if (value < 1) {
+		pg.innerHTML = 5;
+	} else if (value < 5) {
+		pg.innerHTML = 20;
+	} else if (value < 10) {
+		pg.innerHTML = 40;
+	} else if (value < 15) {
+		pg.innerHTML = 65;
+	} else if ( value < 20) {
+		pg.innerHTML = 100;
+	} else if (value < 25) {
+		pg.innerHTML = 145;
+	} else if (value < 30) {
+		pg.innerHTML = 210;
+	}
 }
